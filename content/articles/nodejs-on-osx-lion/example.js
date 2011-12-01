0a9a354e5e21cf0bc5fe3b8cdf94e35d088e2ce0
AJ ONeal <coolaj86@gmail.com>
2011-11-30 23:12:22 -0700

    added spaced to see if that fixes the code block probelm

--- content/articles/nodejs-on-osx-lion/example.js
+++ content/articles/nodejs-on-osx-lion/example.js
@@ -2,12 +2,12 @@
   "use strict";
   // Always use strict mode.
   // Friends don't let friends code crap!
-
+  
   // Don't ever use globals!
   // ...but if you do, do it right!
   var global = Function('return this')()
     , msg = 'Hello World!'
     ;
-
+  
   console.log(msg);
 }());
